97d87218dfc024f9f3f3da0ece50c5bbd26d3680
joebm2018 <volado23@gmail.com>
2019-10-23 22:07:19 -0500

    server
    localhost

--- semana 22/proyectito/index.ts
+++ semana 22/proyectito/index.ts
@@ -1,7 +1,19 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const app = express();
+// MANEJANDO PETICIONES
 app.get('/api',(req,res)=>{
-    res.send("Hola soy la API en TS");
+
+    console.log(req.query);
+    //MANDANDO EL CODIGO DE ESTADO
+    res.status(201);
+    //MANDANDO LA RESPUESTA
+    res.send({
+        //"Hola soy la API en TS"
+        "operation":"rxito",
+        "message":"factura creada con exito"
+    });
 });
 const port= 3000;
+//ESCUCHANDO PETICIONES
 app.listen(port,()=>console.log("Servidor corriendo en el puerto "+port));
